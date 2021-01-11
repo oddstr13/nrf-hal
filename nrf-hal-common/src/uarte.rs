d1894b7f5847eebae20c0ea9bb977112a147b2cd
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-01-11 10:46:25 +0100

    Fix conversion

--- nrf-hal-common/src/uarte.rs
+++ nrf-hal-common/src/uarte.rs
@@ -425,7 +425,7 @@ where
 {
     _marker: core::marker::PhantomData<T>,
     tx_buf: &'a mut [u8],
-    written: u16,
+    written: usize,
 }
 
 /// Interface for the RX part of a UART instance that can be used independently of the TX part.
@@ -560,9 +560,8 @@ pub mod serial {
                 return Err(nb::Error::WouldBlock);
             }
 
-            let written = self.written as usize;
-            if written < self.tx_buf.len() {
-                self.tx_buf[written] = b;
+            if self.written < self.tx_buf.len() {
+                self.tx_buf[self.written] = b;
                 self.written += 1;
                 Ok(())
             } else {
@@ -634,7 +633,7 @@ pub mod serial {
                 uarte
                     .txd
                     .maxcnt
-                    .write(|w| unsafe { w.maxcnt().bits(self.written) });
+                    .write(|w| unsafe { w.maxcnt().bits(self.written as _) });
 
                 // Start UARTE Transmit transaction.
                 // `1` is a valid value to write to task registers.
